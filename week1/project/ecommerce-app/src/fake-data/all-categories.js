f4a38795b852fd8f5b9323280500a19e31f9797c
Hadeel Murshed <131537843+Hadeel-Mur@users.noreply.github.com>
2023-10-31 15:30:54 +0100

    correct some codes

--- week1/project/ecommerce-app/src/fake-data/all-categories.js
+++ week1/project/ecommerce-app/src/fake-data/all-categories.js
@@ -1,11 +1,3 @@
-
-// export default [
-//   "FAKE: electronics",
-//   "FAKE: jewelery",
-//   "FAKE: men's clothing",
-//   "FAKE: women's clothing",
-// ];
-
 const categories = [
   "FAKE: electronics",
   "FAKE: jewelery",
@@ -13,12 +5,5 @@ const categories = [
   "FAKE: women's clothing",
 ];
 
-// const categories = [
-//   {id : 1, category: "FAKE: electronics",
-//    id : 2, category: "FAKE: jewelery",
-//    id : 3, category: "FAKE: men's clothing",
-//    id : 4, category: "FAKE: women's clothing",
-//   }
-// ]
 
 export default categories;
